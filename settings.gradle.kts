f88667cd6bd534cbdf538f1fa3e0577748c818c8
Fajar Alam <sidik3998@gmail.com>
2024-06-05 18:57:57 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UTSAndroid-IF9K-10121919-FajarAlam"
+include(":app")
+ 
